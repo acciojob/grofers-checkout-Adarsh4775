4cbd09b41cc452a2795b22d8e35bfab81626b0ba
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-10 13:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 const getSumBtn = document.createElement("button");
-getSumBtn.textContent = "Get Total Price";
-
+getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-let total = 0;
+const prices = document.querySelectorAll('.price')
+
 const getSum = () => {
- const prices = document.querySelectorAll(".prices");
-	prices.forEach((PriceElement) => {
-	const priceValue = parseFloat(PriceElement.textContent);
-    if(!isNaN(priceValue))
-	{
-		 total += priceValue;
-	}
-	});
-
-	const newRow = document.createElement("tr");
-	const newCell = document.createElement("td");
-
-	newCell.textContent = total ;
-	newRow.appendChild(newCell);
-
-	const table = document.querySelector("table");
-	table.appendChild(newRow);
-	
+//Add your code here
+	let total = 0  
+	prices.forEach(el=>{
+		total+=Number(el.innerText)
+	})
+	const newRow = document.createElement('tr')
+	newRow.innerText = total
+	newRow.id = "ans"
+	const table = document.querySelectorAll('table')[0]
+	table.appendChild(newRow)
 };
 
 getSumBtn.addEventListener("click", getSum);
-
